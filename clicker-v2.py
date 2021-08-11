939e1d384061f53bf233196b2ec3a9334956fb78
Xavier Loo <xavier@senangprint.com>
2021-08-12 04:31:41 +0800

    Update clicker-v2.py

--- clicker-v2.py
+++ clicker-v2.py
@@ -7,4 +7,4 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Edge("lib\edgedriver_win32\msedgedriver.exe")
 
 driver.get("https://popcat.click/")
-driver.execute_script('var event=new KeyboardEvent("keydown",{key:"g",ctrlKey:!0});setInterval(function(){for(i=0;i<1000;i++)document.dispatchEvent(event)},0);');
+driver.execute_script('var event=new KeyboardEvent("keydown",{key:"g",ctrlKey:!0});setInterval(function(){for(i=0;i<100;i++)document.dispatchEvent(event)},0);');
