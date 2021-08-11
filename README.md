15f731e706e31c2bd6b6319de045a617ee14d93b
Xavier Loo <xavier@senangprint.com>
2021-08-12 03:45:11 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# popcat-clicker
+ 
---
+++ run.py
@@ -0,0 +1,24 @@
+import os
+import time
+import itertools
+
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Edge("lib\edgedriver_win32\msedgedriver.exe")
+
+def navigate(url):
+    driver.get(url)
+
+def click(target):
+    WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.ID, target)))
+    driver.find_element_by_id(target).click()
+
+navigate("https://popcat.click/")
+
+while True:
+    click("app")
