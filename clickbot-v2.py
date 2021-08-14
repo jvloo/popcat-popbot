5f4dba6c77ed403925df64df1e6f0d9535ec8c47
Xavier Loo <xavier@senangprint.com>
2021-08-14 22:14:59 +0800

    Update clickbot-v2.py

--- clickbot-v2.py
+++ clickbot-v2.py
@@ -2,7 +2,5 @@ from selenium import webdriver
 
 driver = webdriver.Edge("lib\edgedriver_win32\msedgedriver.exe")
 driver.get("https://popcat.click/")
-
-while True:
-    driver.execute_script('const app=document.querySelector("#app").__vue__;function click(){app.accumulator=800,app.bot=!1,app.sequential_max_pops=0,pop()}function pop(){for(var e=0;e<=800;e++)setTimeout(()=>{document.dispatchEvent(new KeyboardEvent("keydown",{key:"g",ctrlKey:!0}))},5)}document.cookie="country=MY;expires=Sat, 31 Dec 2022 12:00:00 UTC;path=/",document.dispatchEvent(new KeyboardEvent("keydown",{key:"g",ctrlKey:!0})),setInterval(()=>{click()},29e3);')
+driver.execute_script('const app=document.querySelector("#app").__vue__;function click(){app.accumulator=800,app.bot=!1,app.sequential_max_pops=0,pop()}function pop(){for(var e=0;e<=800;e++)setTimeout(()=>{document.dispatchEvent(new KeyboardEvent("keydown",{key:"g",ctrlKey:!0}))},5)}document.cookie="country=MY;expires=Sat, 31 Dec 2022 12:00:00 UTC;path=/",document.dispatchEvent(new KeyboardEvent("keydown",{key:"g",ctrlKey:!0})),setInterval(()=>{click()},29e3);')
 
